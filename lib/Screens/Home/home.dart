777ff5755a4f363cefe7363cf7478573fbf63eb8
aayurt <ayurtshrestha@gmail.com>
2020-06-23 10:25:23 +0545

    dar home

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -18,6 +18,56 @@ class Home extends StatelessWidget {
 
         return false;
       },
+      child: Scaffold(
+        appBar: AppBar(title: Text("WELCOME")),
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: Text('Logout'),
+                onTap: () async {
+                  await storage.delete(key: "jwt");
+                  // await storage.write(key: "jwt", value: null);
+                  Navigator.pushNamed(context, 'login',
+                      arguments: "I am Passed from Login");
+
+                  // Add your onPressed code here!
+                },
+                // Update the state of the app.
+                // ...
+              ),
+            ],
+          ), // Populate the Drawer in the next step.
+        ),
+        body: Center(child: Text("I am at Home")),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () async {
+        //     await storage.delete(key: "jwt");
+        //     // await storage.write(key: "jwt", value: null);
+        //     Navigator.pushNamed(context, 'login',
+        //         arguments: "I am Passed from Login");
+
+        //     // Add your onPressed code here!
+        //   },
+        //   child: Icon(Icons.navigation),
+        //   backgroundColor: Colors.green,
+        // ),
+      ),
     );
   }
 }
